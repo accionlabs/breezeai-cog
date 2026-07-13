3dd0648defb57ed9a02aae692acec72ce187a4d4
Kannan V M <kannan@disroot.org>
2026-07-13 16:51:47 +0530

    feat: add upload support to CLI

--- src/breezeai_cog/services/__init__.py
+++ src/breezeai_cog/services/__init__.py
@@ -4,5 +4,6 @@ server. Wires config + core pipeline + emit."""
 from __future__ import annotations
 
 from .analysis import AnalysisResult, AnalysisService
+from .upload import upload_ontology
 
-__all__ = ["AnalysisService", "AnalysisResult"]
+__all__ = ["AnalysisService", "AnalysisResult", "upload_ontology"]
